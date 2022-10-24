541b69e9e561c492d670339848f622082731f2ea
Icaro Albar <icaro.albar@live.com>
2022-10-24 10:21:05 -0300

    SEO Configurado

---
+++ components/SeoConfig.jsx
@@ -0,0 +1,7 @@
+const title = 'Plano de Saúde Sulamérica Rio'
+const description = 'Todos os planos de saúde Sulamérica e simulador de planos de saúde online'
+
+const SEO = {
+    title,
+    description
+}
