585abee2b119c07a821fabbac03d154a6ae822bc
Icaro Albar <icaro.albar@live.com>
2022-10-24 12:23:57 -0300

    Info1 configurado

---
+++ components/Info1.jsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { Link } from "react-scroll";
+
+export default function Info1() {
+    return (
+        <div className="bg-gray-50">
+            <div className="grid grid-cols-2 gap-x-4">
+                <div className="col-span-2 lg:col-span-1 bg-right text-center">
+                    <Image
+                        src="/images/imageInfo1.png"
+                        //layout='responsive'
+                        width={420}
+                        height={700}
+                        alt="Logo homem de braço cruzado"
+                    />
+                </div>
+                <div className="col-span-2 lg:col-span-1 w-3/4 mx-auto text-center py-16">
+
+                    <p className="uppercase border-b-4 text-secondary pb-2 text-lg border-primary text-center lg:text-left">simulador de planos Sulamérica saúde rio</p>
+                    <h1 className="text-3xl mt-10 font-light lg:text-left text-primary">Planos a partir de</h1>
+
+                    <div className="text-primary font-bold text-center">
+                        <span className="text-3xl">R$</span><span className="text-9xl sm:text-super">145</span>
+                        <span className="relative bottom-16 sm:bottom-28 text-3xl">,00</span>
+                    </div>
+
+                    <p className="text-3xl font-light lg:text-end mb-5 lg:pr-3 text-primary">Economize até 40% </p>
+                    <p className="text-center text-gray-600 text-xl">Valores para o plano de saúde na linha empresarial Amil Fácil.</p>
+                    <p className="text-center text-gray-600 text-xl">*De 0 à 18 Anos para o Rio de Janeiro</p>
+
+                    <div className="mt-10">
+                        <Link
+
+                            to="contato"
+                            smooth={true}
+                            offset={50}
+                            duration={1000}
+                            className="px-10 cursor-pointer py-3 font-medium text-md border-2 bg-primary hover:bg-white border-primary text-white hover:text-primary rounded-xl uppercase transition duration-300 ease-in-out">preço rápido
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,12 +3,14 @@ import Contato from '../components/Contato'
 import Faixa from '../components/Faixa'
 import Footer from '../components/Footer'
 import Info3 from '../components/Info3'
+import Info1 from '../components/Info1'
 import Nav from '../components/Nav'
 
 export default function Home() {
   return(
     <>
       <Nav />
+      <Info1 />
       <Info3 />
       <Faixa />
       <Contato button="enviar" display='hidden' bg={`bg-white`} />
