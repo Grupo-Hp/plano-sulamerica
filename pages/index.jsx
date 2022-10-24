55fdca847c91493de3d10db11de58cb333b899af
Icaro Albar <icaro.albar@live.com>
2022-10-24 12:34:27 -0300

    Info2 configurado

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Contato from '../components/Contato'
 import Faixa from '../components/Faixa'
 import Footer from '../components/Footer'
 import Info3 from '../components/Info3'
+import Info2 from '../components/Info2'
 import Info1 from '../components/Info1'
 import Nav from '../components/Nav'
 
@@ -11,6 +12,7 @@ export default function Home() {
     <>
       <Nav />
       <Info1 />
+      <Info2 />
       <Info3 />
       <Faixa />
       <Contato button="enviar" display='hidden' bg={`bg-white`} />
