3641192501e2aa3cb24a87246654abd40bd9faee
Icaro Albar <icaro.albar@live.com>
2022-10-24 11:28:18 -0300

    Footer configurado

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <div className="bg-primary text-white p-3 text-center">
+            &copy; 2022 Plano de Saúde Sulamérica Rio - <a href="https://hpcap.com.br/" target='blank' className="hover:underline">HP Capital</a>
+        </div>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import Footer from '../components/Footer'
 import Nav from '../components/Nav'
 
 export default function Home() {
   return(
     <>
       <Nav />
+      <Footer />
     </>
   )
 }
