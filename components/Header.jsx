a1a261846e8308020606abd3388c8f5e2a50f2a8
Icaro Albar <45662231+icaroalbar@users.noreply.github.com>
2022-10-26 14:07:39 -0300

    Atualização Rodrigo Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-scroll";
+import { FiPhone } from "react-icons/fi";
 
 export default function Header() {
     return (
@@ -9,6 +10,12 @@ export default function Header() {
                         <h3 className="text-6xl font-light mb-5">Reduza o valor do seu plano de saúde</h3>
                         <p className="text-xl">Em até</p>
                         <h1 className="font-bold text-9xl xl:text-super px-10 mb-5">40%</h1>
+
+                        <ul>
+                            <li className="flex mb-3"><FiPhone className={`text-primary-10 text-4xl`} /><p className="text-4xl">&ensp; 21 4040-4148</p></li>
+                            <li><p className="text-xl lg:text-2xl">Cotação com nossa equipe e 100% Online.</p></li>
+                        </ul>
+
                         <div className="grid sm:flex gap-x-4 text-center">
                             <a href="./contato" className="mt-2 px-10 cursor-pointer py-3 font-medium text-md border-2 border-secondary text-white hover:text-white rounded-xl bg-secondary leading-tight uppercase hover:bg-orange-400 transition duration-300 ease-in-out">simulador online</a>
                             <Link
